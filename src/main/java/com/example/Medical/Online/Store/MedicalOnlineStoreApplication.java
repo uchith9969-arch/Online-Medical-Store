384929029acb9e335c3f32b5d17aeec302f4f415
UV Ranasinghe <Uchith9969@gmail.com>
2026-04-26 17:14:38 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/example/Medical/Online/Store/MedicalOnlineStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.Medical.Online.Store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalOnlineStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalOnlineStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Medical/Online/Store/MedicalOnlineStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Medical.Online.Store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalOnlineStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
